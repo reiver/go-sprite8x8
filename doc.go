d559b89ec39fe5acc645f407ed1f8a47175e5cfe
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-18 11:48:16 -0700

    initial commit

--- doc.go
+++ doc.go
@@ -1,5 +1,12 @@
 /*
 Package sprite8x8 provides a type that represents a 8×8 sprite,
 that seamlessly works with Go's built-in "image", "image/color", and "image/draw" packages.
+
+The slice- and array- indexing of the pixels / pels in sprite is as follows:
+
+	 0  1  2  3
+	 4  5  6  7
+	 8  9 10 11
+	12 13 14 15
 */
 package sprite8x8
